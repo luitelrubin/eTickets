4e50b5dde827b60cb7d36364041e18fb22418097
Rubin <rubinprasadluitel@gmail.com>
2025-12-20 13:43:36 +0545

    Create Actors, Cinemas, Movies and Producers controller skeleton

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,23 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context = null!;
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var actors = await _context.Actors.ToListAsync();
+            return View(actors);
+        }
+
+
+
+    }
+}
---
+++ eTickets/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context = null!;
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies.ToListAsync();
+            return View(movies);
+        }
+    }
+}
