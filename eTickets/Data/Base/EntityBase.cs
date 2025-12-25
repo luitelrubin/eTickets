6ed129844429a5e0dcefdb052310b8bb1f9bce32
Rubin <rubinprasadluitel@gmail.com>
2025-12-25 11:07:11 +0545

    Create parent classes for shared services and entity model properties

---
+++ eTickets/Data/Base/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBase : IEntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _context = null!;
+
+        public EntityBaseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FirstOrDefaultAsync(e => e.Id == id);
+            if (entity != null) _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>().ToListAsync();
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id) => await _context.Set<T>().FirstOrDefaultAsync(e => e.Id == id);
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
