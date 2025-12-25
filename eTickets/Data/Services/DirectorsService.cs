1814a5af9bffc1540da85741a51654b65a64f8c3
Rubin <rubinprasadluitel@gmail.com>
2025-12-25 11:07:28 +0545

    Create services

---
+++ eTickets/Data/Services/DirectorsService.cs
@@ -0,0 +1,12 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+namespace eTickets.Data.Services
+{
+    public class DirectorsService : EntityBaseRepository<Director>, IDirectorsService
+    {
+        public DirectorsService(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ eTickets/Data/Services/MoviesService.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
+{
+    public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
+    {
+        private readonly AppDbContext _context = null!;
+        public MoviesService(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Movie?> GetMovieByIdAsync(int id)
+        {
+            var movie = await _context.Movies
+                .Include(d => d.Director)
+                .Include(c => c.Cinema)
+                .Include(am => am.ActorsMovies).ThenInclude(a => a.Actor)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return movie;
+        }
+    }
+}
