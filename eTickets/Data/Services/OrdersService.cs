f2dea0bd6a9adaca7870edaa455123105f7bcd48
Rubin <rubinprasadluitel@gmail.com>
2025-12-27 22:04:04 +0545

    Create orderservice and its interface

---
+++ eTickets/Data/Services/OrdersService.cs
@@ -0,0 +1,45 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly AppDbContext _context;
+        public OrdersService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Order>> GetOrderByUserIdAsync(string userId)
+        {
+            var userOrders = await _context.Orders.Include(n => n.OrderItems).ThenInclude(n => n.Movie).Where(uo => uo.UserId == userId).ToListAsync();
+            return userOrders;
+        }
+
+        public async Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmail)
+        {
+
+            var order = new Order
+            {
+                UserId = userId,
+                Email = userEmail,
+            };
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+
+            foreach (var item in items)
+            {
+                var orderItem = new OrderItem
+                {
+                    OrderId = order.Id,
+                    MovieId = item.Movie.Id,
+                    Price = item.Movie.Price,
+                    Quantity = item.Quantity
+                };
+                await _context.OrderItems.AddAsync(orderItem);
+                await _context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
