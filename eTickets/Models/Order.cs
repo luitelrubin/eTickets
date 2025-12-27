bb5f85c1d48a75f9b7cfd27e9a9805f812c874d8
Rubin <rubinprasadluitel@gmail.com>
2025-12-27 22:03:14 +0545

    Create order, orderitem and shoppingcartitem models

---
+++ eTickets/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using eTickets.Data.Base;
+
+namespace eTickets.Models
+{
+    public class Order : EntityBase
+    {
+        public string Email { get; set; } = String.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ eTickets/Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using eTickets.Data.Base;
+
+namespace eTickets.Models
+{
+    public class OrderItem : EntityBase
+    {
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null!;
+    }
+}
