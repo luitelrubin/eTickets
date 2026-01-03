d09d3d02b5922b0a029570a646301183ce2f4e32
Rubin <rubinprasadluitel@gmail.com>
2026-01-03 19:15:35 +0545

    Set Movies controller to be the entrypoint

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -33,7 +33,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Movies}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 // Seed database
